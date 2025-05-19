f0388dab8e4bb95fc71e2d3c94c50658f24e8c93
nk-on <2nkoz@proton.me>
2025-05-18 23:09:19 -0700

    Style:implementing dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 function App() {
+  const [dark,setDark] = useState(false)
   return (
     <>
-      <div className="w-[100vw] h-[100vh]">
-        <Header></Header>
+      <div className={`w-[100vw] h-[100vh] ${dark ? 'dark':''}`}>
+        <Header  setDark = {setDark}></Header>
       </div>
     </>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
-function Header() {
+interface HeaderProps {
+  setDark:React.Dispatch<React.SetStateAction<boolean>>
+}
+function Header({setDark}:HeaderProps) {
   return (
-    <div className='w-[100%] h-[10%] bg-[#fff] shadow-custom-light flex justify-between items-center px-[30px]'>
+    <div className='w-[100%] h-[10%] bg-[#fff] dark:text-[#fff]  dark:bg-[#2B3844] shadow-custom-light flex justify-between items-center px-[30px]'>
       <h2 className='text-[#111517] font-bold'>Where in the world?</h2>
-      <div className='flex gap-[10px]'>
+      <div className='flex gap-[10px]' onClick={()=>{
+        setDark(dark => !dark)
+      }}>
         <h3>Dark Mode</h3>
         <img src='public/darkModeIcon.svg' alt='dark-mode' className='w-[20px] h-[20px]' />
       </div>
