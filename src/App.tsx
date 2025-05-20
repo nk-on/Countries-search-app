51458d64a7eed2657e9511428f1a275ead07c619
nk-on <2nkoz@proton.me>
2025-05-19 21:11:36 -0700

    Style:adding and styling showcase component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
+import Showcase from "./showcase";
 function App() {
   const [dark,setDark] = useState(false)
   return (
     <>
       <div className={`w-[100vw] h-[100vh] ${dark ? 'dark':''}`}>
         <Header  setDark = {setDark}></Header>
+        <Showcase />
       </div>
     </>
   );
