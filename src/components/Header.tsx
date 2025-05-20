e0dfdd50c6917314cf13802171bc525eaef0d944
nk-on <2nkoz@proton.me>
2025-05-19 22:05:38 -0700

    Moving header to components folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+interface HeaderProps {
+  setDark:React.Dispatch<React.SetStateAction<boolean>>
+}
+function Header({setDark}:HeaderProps) {
+  return (
+    <div className='w-[100%] h-[10%] bg-[#fff] dark:text-[#fff]  dark:bg-[#2B3844] shadow-custom-light flex justify-between items-center px-[30px]'>
+      <h2 className='text-[#111517] font-bold dark:text-[#fff]'>Where in the world?</h2>
+      <div className='flex gap-[10px]' onClick={()=>{
+        setDark(dark => !dark)
+      }}>
+        <h3>Dark Mode</h3>
+        <img src='public/darkModeIcon.svg' alt='dark-mode' className='w-[20px] h-[20px]' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
