507800b191698b45c4e0835bdce3316fa539f4ee
nk-on <2nkoz@proton.me>
2025-05-20 23:01:36 -0700

    Style:fixing options list styling

--- src/components/selectionForm/OptionsList.tsx
+++ src/components/selectionForm/OptionsList.tsx
@@ -1,7 +1,7 @@
 import { continents } from "../../Continents";
 export default function OptionsList() {
   return (
-    <div className="w-[200px]  flex lg:flex-col px-[10px] mt-[20px] py-[10px] dark:bg-[#2B3844] dark:text-[#fff]">
+    <div className="w-[200px]   flex flex-col px-[10px] mt-[20px] py-[10px] dark:bg-[#2B3844] dark:text-[#fff]">
       {continents.map((continent) => {
         return <h2>{continent.name}</h2>;
       })}
