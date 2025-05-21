b2ec8f2257c47ce1fc2605b89dbb968850b5018a
nk-on <2nkoz@proton.me>
2025-05-20 22:42:54 -0700

    Style:fixing design in search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,14 @@
-
-import React from 'react'
-
 export default function SearchBar() {
   return (
-    <div className='w-[480px] h-[80px]  dark:bg-[#2B3844] bg-[#fff] shadow-search-bar flex justify-between items-center shadow-custom-light'>
-        <div>
-            <img src='public/search.svg' />
-        </div>
-      <input type='text' placeholder='search for a country' className='h-[100%] w-[90%]' />
+    <div className="px-[30px] lg:w-[480px]  h-[70px]   dark:bg-[#2B3844] bg-[#fff]  flex justify-between items-center shadow-custom-light">
+      <div>
+        <img src="public/search.svg" />
+      </div>
+      <input
+        type="text"
+        placeholder="search for a country"
+        className="h-[100%] w-[90%] outline-none dark:bg-[#2B3844]"
+      />
     </div>
-  )
+  );
 }
