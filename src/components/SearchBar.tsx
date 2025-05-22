24bf41df9b9640c6c3893c6820b2cba2b2c275a3
nk-on <2nkoz@proton.me>
2025-05-21 22:03:46 -0700

    Feat:setting search query variable when clicking search button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,24 @@
-export default function SearchBar() {
+import { useRef } from "react";
+
+interface searchBar {
+  setQuery: React.Dispatch<React.SetStateAction<string>>
+}
+export default function SearchBar({setQuery}:searchBar) {
+  const typedString = useRef<string>('');
   return (
     <div className="px-[30px] lg:w-[480px]  h-[70px]   dark:bg-[#2B3844] bg-[#fff]  flex justify-between items-center shadow-custom-light">
       <div>
-        <img src="public/search.svg" />
+        <img src="public/search.svg" onClick={()=>{
+          setQuery(typedString.current )
+        }} />
       </div>
       <input
         type="text"
         placeholder="search for a country"
         className="h-[100%] w-[90%] outline-none dark:bg-[#2B3844]"
+        onChange={(e)=>{
+          typedString.current = e.target.value;
+        }}
       />
     </div>
   );
--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,11 +1,12 @@
 import SearchBar from "./SearchBar"
 import SelectForm from "./selectionForm/SelectForm"
-
+import { useState } from "react"
 function Showcase() {
+  const [query,setQuery] = useState<string>('')
   return (
     <div className='w-[100vw] h-[90%] dark:bg-[#202C36] bg-[#FAFAFA]'>
       <div className="w-[100%] relative top-[30px] flex flex-col gap-[30px] lg:flex-row justify-between lg:items-center px-[30px]">
-        <SearchBar />
+        <SearchBar setQuery = {setQuery} />
         <SelectForm />
       </div>
     </div>
