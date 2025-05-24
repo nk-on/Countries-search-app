fed4c6acb22101744a2061733c2667f83b98791f
nk-on <2nkoz@proton.me>
2025-05-24 14:02:15 -0700

    Feat:displaying data on the country page

--- src/components/CountryPage.tsx
+++ src/components/CountryPage.tsx
@@ -1,7 +1,40 @@
-export default function CountryPage() {
+import { useContext } from "react";
+import { DataContext } from "../DataContext";
+import { useParams } from "react-router";
+function DataContainer({ title, data }) {
   return (
-    <div>
-        
+    <div className="flex flex-row">
+      <h3>{title}:</h3>
+      <span>{data}</span>
     </div>
-  )
+  );
+}
+export default function CountryPage() {
+  const { data } = useContext(DataContext);
+  const { id } = useParams();
+  const country = data?.find((element) => Number(id) === element.area);
+  const name = country?.altSpellings[1];
+  const flag = country?.flags.png;
+  const nativeName = country?.altSpellings[2];
+  const population = country?.population;
+  const region = country?.region;
+  const subRegion = country?.subregion;
+  const capital = country?.capital;
+  const countryCode = country?.altSpellings[0];
+  return (
+    <div className="w-[100%] h-[100%]">
+      <div>
+        <img src={flag}></img>
+      </div>
+      <div>
+        <DataContainer title="Country Code" data={countryCode} />
+        <DataContainer title="Country Name" data={name} />
+        <DataContainer title="Native Name" data={nativeName} />
+        <DataContainer title="Population" data={population} />
+        <DataContainer title="Region" data={region} />
+        <DataContainer title="Subregion" data={subRegion} />
+        <DataContainer title="Capital" data={capital} />
+      </div>
+    </div>
+  );
 }
