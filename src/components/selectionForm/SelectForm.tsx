71cb2eca4122d329b8579564bd0f254632db5c4c
nk-on <2nkoz@proton.me>
2025-06-09 22:01:28 -0700

    Style:changing expand icon for the dark mode

--- src/components/selectionForm/SelectForm.tsx
+++ src/components/selectionForm/SelectForm.tsx
@@ -6,9 +6,9 @@ export default function SelectForm() {
   return (
     <div>
       <div className={`w-[200px]  shadow-custom-light flex  flex-col justify-between items-center ${optionsListVisable && 'mt-[10%]'} cursor-pointer`}>
-        <div className="flex justify-between w-[100%] bg-[#fff] dark:bg-[#2B3844] p-[10px] ">
+        <div className="flex justify-between w-[100%] bg-[#fff] dark:bg-[#2B3844] p-[10px]">
           <h1 className="dark:text-[#fff]">Filter by region</h1>
-          <img src="./expand-more.svg" className="w-[20px] h-[20px]" onClick={()=> setOptionsListVisable(optionsListVisable => !optionsListVisable)} />
+          <div  className="w-[20px] h-[20px] mt-[7px]  bg-[url('./expand-more.svg')] dark:bg-[url('./Expand.svg')]  bg-no-repeat" onClick={()=> setOptionsListVisable(optionsListVisable => !optionsListVisable)} ></div>
         </div>
       </div>
       {optionsListVisable && <OptionsList />}
