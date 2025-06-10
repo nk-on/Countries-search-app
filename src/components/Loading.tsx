a88a2a709a546ecc0ae1315b70739aa7b4c9ad49
nk-on <2nkoz@proton.me>
2025-06-09 23:19:28 -0700

    updating image pathes

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ function Loading() {
     <div className="w-[100%] h-[100%] mt-[25%] flex  justify-center items-center dark:text-[#fff]">
       <div className="flex flex-col h-[100%] items-center">
         <video id="Loading-animation" autoPlay>
-          <source src="./AnimationLoading.webm" type="video/webm"></source>
+          <source src="/AnimationLoading.webm" type="video/webm"></source>
         </video>
         <h1>Loading</h1>
       </div>
