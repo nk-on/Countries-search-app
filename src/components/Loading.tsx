7107684e9bebbb1c45e21c0aa849561e2c5aa997
nk-on <2nkoz@proton.me>
2025-06-09 22:40:06 -0700

    Fixing image path

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,9 @@
 function Loading() {
   return (
-    <div className="w-[100%] h-[100%] flex justify-center items-center">
+    <div className="w-[100%] h-[100%]  flex justify-center items-center">
       <div className="flex flex-col items-center">
         <video id="Loading-animation" autoPlay>
-          <source src="public/AnimationLoading.webm" type="video/webm"></source>
+          <source src="./AnimationLoading.webm" type="video/webm"></source>
         </video>
         <h1>Loading</h1>
       </div>
